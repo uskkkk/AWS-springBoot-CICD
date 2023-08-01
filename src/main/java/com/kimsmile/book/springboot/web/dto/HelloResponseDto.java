8f793b8edd8bf596f63667f47098b790b0570068
uskkkk <dntjd4562@gmail.com>
2023-08-01 10:14:56 +0900

    fw) 단위테스트 진행

---
+++ src/main/java/com/kimsmile/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.kimsmile.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor // 선언된 모든 final 필드가 포함된 생성자를 생성해줌
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
