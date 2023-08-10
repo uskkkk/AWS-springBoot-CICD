5979440b170e9bf2b7301cc241f16862a5b52068
uskkkk <dntjd4562@gmail.com>
2023-08-10 17:42:31 +0900

    fw) GenerationType 변경

--- src/main/java/com/kimsmile/book/springboot/domain/user/User.java
+++ src/main/java/com/kimsmile/book/springboot/domain/user/User.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 public class User extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
